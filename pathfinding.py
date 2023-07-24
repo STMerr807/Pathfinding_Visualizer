c18172227b13bd4b0a199c4c53d3c748a256a486
STMerr807 <95002818+STMerr807@users.noreply.github.com>
2023-07-24 14:54:23 -0400

    Separated pathfinding, improved efficiency

--- pathfinding.py
+++ pathfinding.py
@@ -1,3 +1,4 @@
+import pygame
 from queue import PriorityQueue
 
 
@@ -8,80 +9,36 @@ class Node:
         self.col = col  # Column position of node
         self.x = row * width  # X position of node
         self.y = col * width  # Y position of node
-        self.color = WHITE  # Color of node
+        self.state = "empty"  # Stores what state the node is currently in
         self.neighbors = []  # List of neighbor nodes
         self.width = width  # Width of node
         self.total_rows = total_rows  # Total rows in grid
+
+    # Check the state of the node
+    def get_node_state(self):
+        return self.state
 
     # Get position
     def get_pos(self):
         return self.row, self.col
 
-    # Check if node is closed
-    def is_closed(self):
-        return self.color == RED
-
-    # Check if node is open
-    def is_open(self):
-        return self.color == GREEN
-
-    # Check if node is barrier
-    def is_barrier(self):
-        return self.color == BLACK
-
-    # Check if node is start
-    def is_start(self):
-        return self.color == ORANGE
-
-    # Check if node is an end node
-    def is_end(self):
-        return self.color == CYAN
-
-    # Reset node
-    def reset(self):
-        self.color = WHITE
-
-    # Make start node
-    def make_start(self):
-        self.color = ORANGE
-
-    # Make node closed
-    def make_closed(self):
-        self.color = RED
-
-    # Make node open
-    def make_open(self):
-        self.color = GREEN
-
-    # Make node barrier
-    def make_barrier(self):
-        self.color = BLACK
-
-    # Make node end
-    def make_end(self):
-        self.color = CYAN
-
-    # Make node path
-    def make_path(self):
-        self.color = YELLOW
-
-    # Draw node
-    def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+    # Set the state of the node
+    def set_node_state(self, state):
+        self.state = state
 
     # Update neighbors
     def update_neighbors(self, grid):
         self.neighbors = []
-        if self.row < self.total_rows - 1 and not grid[self.row + 1][self.col].is_barrier():  # Down
+        if self.row < self.total_rows - 1 and not (grid[self.row + 1][self.col].get_node_state() == "barrier"):  # Down
             self.neighbors.append(grid[self.row + 1][self.col])
 
-        if self.row > 0 and not grid[self.row - 1][self.col].is_barrier():  # Up
+        if self.row > 0 and not (grid[self.row - 1][self.col].get_node_state() == "barrier"):  # Up
             self.neighbors.append(grid[self.row - 1][self.col])
 
-        if self.col < self.total_rows - 1 and not grid[self.row][self.col + 1].is_barrier():  # Right
+        if self.col < self.total_rows - 1 and not (grid[self.row][self.col + 1].get_node_state() == "barrier"):  # Right
             self.neighbors.append(grid[self.row][self.col + 1])
 
-        if self.col > 0 and not grid[self.row][self.col - 1].is_barrier():  # Left
+        if self.col > 0 and not (grid[self.row][self.col - 1].get_node_state() == "barrier"):  # Left
             self.neighbors.append(grid[self.row][self.col - 1])
 
     def __lt__(self, other):
@@ -95,15 +52,14 @@ def heuristic(p1, p2):
     return abs(x1 - x2) + abs(y1 - y2)
 
 
-def reconstruct_path(came_from, current, draw):
+def reconstruct_path(came_from, current):
     while current in came_from:
         current = came_from[current]  # Get current node
-        current.make_path()  # Make current node path
-        draw()  # Draw display window
+        current.set_node_state('path')  # Make current node path
 
 
 # A* algorithm
-def algorithm(draw, grid, start, end):
+def algorithm(grid, start, end):
     count = 0
     open_set = PriorityQueue()  # Create an open set
     open_set.put((0, count, start))  # Add start node to open set
@@ -115,6 +71,8 @@ def algorithm(draw, grid, start, end):
 
     open_set_hash = {start}  # Keep track of items in PriorityQueue
 
+    visited = []  # List of visited nodes
+
     while not open_set.empty():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -124,9 +82,10 @@ def algorithm(draw, grid, start, end):
         open_set_hash.remove(current)  # Remove current node from open set
 
         if current == end:
-            reconstruct_path(came_from, end, draw)  # Reconstruct path
-            end.make_end()  # Make end node
-            return True  # If current node is end node, return true
+            reconstruct_path(came_from, end)  # Reconstruct path
+            end.set_node_state('end')  # Make end node
+            visited.append(current)  # Add current node to visited list
+            return visited  # Return list of visited nodes
 
         for neighbor in current.neighbors:
             temp_g_score = g_score[current] + 1  # Calculate temp g score
@@ -139,9 +98,21 @@ def algorithm(draw, grid, start, end):
                     count += 1
                     open_set.put((f_score[neighbor], count, neighbor))  # Add neighbor to open set
                     open_set_hash.add(neighbor)  # Add neighbor to open set hash
-                    neighbor.make_open()  # Make neighbor open
-
-        draw()  # Draw display window
+                    neighbor.set_node_state('open')  # Make neighbor open
+                    visited.append(neighbor)  # Add neighbor to visited nodes
 
         if current != start:  # If current node is not start node
-            current.make_closed()  # Make current node closed
+            current.set_node_state('closed')  # Make current node closed
+
+        """
+        To visualize the algorithm
+        uncomment the statements that follow
+        and comment out the return statement after them
+        """
+        # yield current  # Yield current node
+
+    # for row in grid:
+    #     for node in row:
+    #         yield node  # If no path is found, yield all visited nodes
+
+    return visited  # Return list of visited nodes if no path found
