bb886118d8f1fbdf0931dddd9771accb50d21f58
STMerr807 <95002818+STMerr807@users.noreply.github.com>
2023-07-24 23:49:41 -0400

    Efficiency & bug fixes

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pygame
 import pathfinding
 
 # Set up the window
-WIDTH = 800
-WIN = pygame.display.set_mode((WIDTH, WIDTH))
+WIDTH, HEIGHT = 1600, 800
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("A* Path Finding Algorithm")
+
+# Define Tile Size in pixels
+TILE_SIZE = 16
 
 # Colors
 RED = (165, 40, 30)
@@ -21,28 +24,27 @@ CYAN = (70, 115, 145)
 
 
 # Create the grid
-def make_grid(rows, width):
+def make_grid(rows):
     grid = []
-    gap = width // rows  # Width of each node
     for i in range(rows):
         grid.append([])
         for j in range(rows):
-            node = pathfinding.Node(i, j, gap, rows)  # Create node
+            node = pathfinding.Node(i, j)  # Create node
             grid[i].append(node)  # Add node to grid
     return grid
 
 
 # Draw grid lines
-def draw_grid(win, rows, width):
-    gap = width // rows  # Width of each node
+def draw_grid(w_size, rows, w_width):
+    node_width = w_width // rows  # Width of each node
     for i in range(rows):
-        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))  # Horizontal lines
+        pygame.draw.line(w_size, GREY, (0, i * node_width), (w_width, i * node_width))  # Horizontal lines
     for j in range(rows):
-        pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))  # Vertical Lines
+        pygame.draw.line(w_size, GREY, (j * node_width, 0), (j * node_width, w_width))  # Vertical Lines
 
 
 # Draw the display window
-def draw(win, grid, rows, width):
+def draw(w_size, grid, rows, w_width):
     colors = {
         "empty": WHITE,
         "open": GREEN,
@@ -53,62 +55,64 @@ def draw(win, grid, rows, width):
         "path": YELLOW,
     }
 
-    win.fill(WHITE)  # Fill window with white
+    w_size.fill(WHITE)  # Fill window with white
     for row in grid:
         for node in row:
-            pygame.draw.rect(win, colors[node.state], (node.x, node.y, node.width, node.width))  # Draw node
-    draw_grid(win, rows, width)  # Draw grid lines
+            pygame.draw.rect(w_size, colors[node.state.name.lower()], ((node.row * TILE_SIZE), (node.col * TILE_SIZE), TILE_SIZE, TILE_SIZE))  # Draw node
+    draw_grid(w_size, rows, w_width)  # Draw grid lines
     pygame.display.update()  # Update display
 
 
 # Get node clicked on
-def get_clicked_pos(pos, rows, width):
-    gap = width // rows  # Width of each node
+def get_clicked_pos(pos, rows, w_width):
+    node_width = w_width // rows  # Width of each node
     y, x = pos  # Get position of mouse
-    row = y // gap  # Get row of node
-    col = x // gap  # Get column of node
+    row = y // node_width  # Get row of node
+    col = x // node_width  # Get column of node
     return row, col
 
 
 # Main function
 # noinspection PyUnresolvedReferences
-def main(win, width):
-    rows = 50  # Number of rows
-    grid = make_grid(rows, width)  # Create grid
+def main(w_size=WIN, w_width=WIDTH, tile_size=TILE_SIZE):
+    total_rows = w_width // tile_size  # Number of rows
+    grid = make_grid(total_rows)  # Create grid
     start = None  # Start node
+    start_save = None  # Save location of start node
     end = None  # End node
     run = True  # Run flag
 
     # Main loop
     while run:
-        draw(win, grid, rows, width)  # Draw display window
+        draw(w_size, grid, total_rows, w_width)  # Draw display window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False  # Quit if user closes window
 
             if pygame.mouse.get_pressed()[0]:  # On left mouse click
                 pos = pygame.mouse.get_pos()  # Get position of mouse
-                row, col = get_clicked_pos(pos, rows, width)  # Get node clicked on
-                if 0 <= row < rows and 0 <= col < rows:
+                row, col = get_clicked_pos(pos, total_rows, w_width)  # Get node clicked on
+                if 0 <= row < total_rows and 0 <= col < total_rows:
                     node = grid[row][col]  # Index the node
                     if not start and node.get_node_state() != 'end':  # If start node not set and node is not end node
                         start = node  # Set start node
-                        start.set_node_state('start')  # Make start node
+                        start_save = start  # Save location of the start node
+                        start.set_node_state(pathfinding.State.START)  # Make start node
                     elif not end and node.get_node_state() != 'start':  # If end node not set and node is not start node
                         end = node  # Set end node
-                        end.set_node_state('end')  # Make end node
-                    elif node.get_node_state() != 'end' and node.get_node_state() != 'start':  # If node is not start or end
-                        node.set_node_state('barrier')  # Make barrier node
+                        end.set_node_state(pathfinding.State.END)  # Make end node
+                    elif node.get_node_state() != 'end' and node.get_node_state() != 'start':
+                        node.set_node_state(pathfinding.State.BARRIER)  # Make barrier node
                 else:
                     continue
 
             elif pygame.mouse.get_pressed()[2]:  # On right mouse click
                 pos = pygame.mouse.get_pos()  # Get position of mouse
-                row, col = get_clicked_pos(pos, rows, width)  # Get node clicked on
-                if 0 <= row < rows and 0 <= col < rows:
+                row, col = get_clicked_pos(pos, total_rows, w_width)  # Get node clicked on
+                if 0 <= row < total_rows and 0 <= col < total_rows:
                     node = grid[row][col]  # Index the node
                     # noinspection PyUnresolvedReferences
-                    node.set_node_state('empty')  # Reset node
+                    node.set_node_state(pathfinding.State.EMPTY)  # Reset node
                     if node == start:
                         start = None
                     elif node == end:
@@ -118,25 +122,35 @@ def main(win, width):
                 if event.key == pygame.K_SPACE and start and end:
                     for row in grid:
                         for node in row:
-                            node.update_neighbors(grid)  # Update neighbors
+                            node.update_neighbors(grid, total_rows)  # Update neighbors
                     """
                     To visualize the algorithm, comment out the following three lines
                     and uncomment the three lines after them
                     """
                     visited = pathfinding.algorithm(grid, start, end)  # Run algorithm
                     for node in visited:
-                        draw(win, grid, rows, width)  # Draw display window
+                        draw(w_size, grid, total_rows, w_width)  # Draw display window
 
                     # for node in algorithm(grid, start, end):  # Iterate over nodes from algorithm
                     #     draw(win, grid, rows, width)  # Draw display window
                     #     pygame.time.delay(5)  # Delay to control visualization speed
 
                 if event.key == pygame.K_c:  # On c key press
-                    start = None
-                    end = None
-                    grid = make_grid(rows, width)
+                    start = start_save  # Reset start node
+                    start.set_node_state(pathfinding.State.START)  # Make start node
+                    reset_flag = False
+                    for row in grid:
+                        for node in row:
+                            if node.get_node_state() in {'open', 'closed', 'path'}:
+                                node.set_node_state(pathfinding.State.EMPTY)
+                                reset_flag = True
+                    if not reset_flag:
+                        # Reset grid
+                        start = None
+                        end = None
+                        grid = make_grid(total_rows)
 
     pygame.quit()
 
 
-main(WIN, WIDTH)
+main()
--- pathfinding.py
+++ pathfinding.py
@@ -1,26 +1,34 @@
-import pygame
 from queue import PriorityQueue
+from enum import Enum
+
+
+OPEN_SET = PriorityQueue()
+G_SCORE = {}
+F_SCORE = {}
+
+
+# State updater class
+class State(Enum):
+    EMPTY = 0
+    START = 1
+    END = 2
+    BARRIER = 3
+    OPEN = 4
+    CLOSED = 5
+    PATH = 6
 
 
 # Node class
 class Node:
-    def __init__(self, row, col, width, total_rows):
+    def __init__(self, row, col):
         self.row = row  # Row position of node
         self.col = col  # Column position of node
-        self.x = row * width  # X position of node
-        self.y = col * width  # Y position of node
         self.neighbors = []  # List of neighbor nodes
-        self.width = width  # Width of node
-        self.total_rows = total_rows  # Total rows in grid
-        self.state = "empty"  # Stores what state the node is currently in
-        """
-        Possible node states:
-        empty, start, end, barrier, open, closed, path
-        """
+        self.state = State.EMPTY  # Stores what state the node is currently in
 
     # Check the state of the node
     def get_node_state(self):
-        return self.state
+        return self.state.name.lower()
 
     # Get position
     def get_pos(self):
@@ -28,18 +36,19 @@ class Node:
 
     # Set the state of the node
     def set_node_state(self, state):
+        assert isinstance(state, State)
         self.state = state
 
     # Update neighbors
-    def update_neighbors(self, grid):
+    def update_neighbors(self, grid, total_rows):
         self.neighbors = []
-        if self.row < self.total_rows - 1 and not (grid[self.row + 1][self.col].get_node_state() == "barrier"):  # Down
+        if self.row < total_rows - 1 and not (grid[self.row + 1][self.col].get_node_state() == "barrier"):  # Down
             self.neighbors.append(grid[self.row + 1][self.col])
 
         if self.row > 0 and not (grid[self.row - 1][self.col].get_node_state() == "barrier"):  # Up
             self.neighbors.append(grid[self.row - 1][self.col])
 
-        if self.col < self.total_rows - 1 and not (grid[self.row][self.col + 1].get_node_state() == "barrier"):  # Right
+        if self.col < total_rows - 1 and not (grid[self.row][self.col + 1].get_node_state() == "barrier"):  # Right
             self.neighbors.append(grid[self.row][self.col + 1])
 
         if self.col > 0 and not (grid[self.row][self.col - 1].get_node_state() == "barrier"):  # Left
@@ -59,54 +68,55 @@ def heuristic(p1, p2):
 def reconstruct_path(came_from, current):
     while current in came_from:
         current = came_from[current]  # Get current node
-        current.set_node_state('path')  # Make current node path
+        current.set_node_state(State.PATH)  # Make current node path
 
 
 # A* algorithm
 def algorithm(grid, start, end):
+    # Initialize open set, g score, f score, and count
+    OPEN_SET.queue.clear()
+    G_SCORE.clear()
+    F_SCORE.clear()
     count = 0
-    open_set = PriorityQueue()  # Create an open set
-    open_set.put((0, count, start))  # Add start node to open set
+    OPEN_SET.put((0, count, start))  # Add start node to open set
     came_from = {}  # Keeps track of where a node came from
-    g_score = {node: float("inf") for row in grid for node in row}  # Tracks shortest dist from start to current node
-    g_score[start] = 0  # Set start node g score to 0
-    f_score = {node: float("inf") for row in grid for node in row}  # F score
-    f_score[start] = heuristic(start.get_pos(), end.get_pos())  # Set start node f score to heuristic
 
+    for row in grid:
+        for node in row:
+            # Initialize g score and f score of all nodes to infinity
+            G_SCORE[node] = float("inf")
+            F_SCORE[node] = float("inf")
+
+    G_SCORE[start] = 0  # Set g score of start node to 0
+    F_SCORE[start] = heuristic(start.get_pos(), end.get_pos())  # Set f score of start node to heuristic
     open_set_hash = {start}  # Keep track of items in PriorityQueue
-
     visited = []  # List of visited nodes
 
-    while not open_set.empty():
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-
-        current = open_set.get()[2]  # Get current node
+    while not OPEN_SET.empty():
+        current = OPEN_SET.get()[2]  # Get current node
         open_set_hash.remove(current)  # Remove current node from open set
 
         if current == end:
             reconstruct_path(came_from, end)  # Reconstruct path
-            end.set_node_state('end')  # Make end node
+            end.set_node_state(State.END)  # Make end node
             visited.append(current)  # Add current node to visited list
             return visited  # Return list of visited nodes
 
         for neighbor in current.neighbors:
-            temp_g_score = g_score[current] + 1  # Calculate temp g score
-
-            if temp_g_score < g_score[neighbor]:
+            temp_g_score = G_SCORE[current] + 1  # Calculate temp g score
+            if temp_g_score < G_SCORE[neighbor]:
                 came_from[neighbor] = current
-                g_score[neighbor] = temp_g_score  # Update g score
-                f_score[neighbor] = temp_g_score + heuristic(neighbor.get_pos(), end.get_pos())  # Update f score
+                G_SCORE[neighbor] = temp_g_score  # Update g score
+                F_SCORE[neighbor] = temp_g_score + heuristic(neighbor.get_pos(), end.get_pos())  # Update f score
                 if neighbor not in open_set_hash:
                     count += 1
-                    open_set.put((f_score[neighbor], count, neighbor))  # Add neighbor to open set
+                    OPEN_SET.put((F_SCORE[neighbor], count, neighbor))  # Add neighbor to open set
                     open_set_hash.add(neighbor)  # Add neighbor to open set hash
-                    neighbor.set_node_state('open')  # Make neighbor open
+                    neighbor.set_node_state(State.OPEN)  # Make neighbor open
                     visited.append(neighbor)  # Add neighbor to visited nodes
 
         if current != start:  # If current node is not start node
-            current.set_node_state('closed')  # Make current node closed
+            current.set_node_state(State.CLOSED)  # Make current node closed
 
         """
         To visualize the algorithm
